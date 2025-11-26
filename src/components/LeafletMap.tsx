c018e86b6d2d839038d31df870ded611da50139a
Tugendboldo <thilopeter@gmail.com>
2025-11-26 16:48:32 +0100

    Fix Leaflet DOM manipulation errors

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -3,7 +3,6 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 // Fix for default markers in Leaflet with Vite
-delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
