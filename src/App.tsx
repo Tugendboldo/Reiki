f29e719228d9cf2eee0919512dcb659aea2df4f3
Tugendboldo <thilopeter@gmail.com>
2025-11-26 18:32:48 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -169,7 +169,7 @@ function App() {
               <img 
                 src="/photo_2025-11-26_18-15-56.jpg" 
                 alt="Erika Natural Healing - Tree of Life Logo" 
-                className="w-48 h-48 mx-auto rounded-full object-cover shadow-2xl"
+                className="w-32 h-32 mx-auto rounded-full object-cover shadow-2xl border-4 border-white/80"
               />
             </div>
             
